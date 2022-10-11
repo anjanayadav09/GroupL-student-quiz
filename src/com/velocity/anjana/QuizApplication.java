adc5328b409bbbd941f12e8429fbb2bad2c6ca3d
Anjana Yadav <yadavanjana3485@gmail.com>
2022-10-11 19:00:30 +0530

    Refactoring

--- src/com/velocity/anjana/QuizApplication.java
+++ src/com/velocity/anjana/QuizApplication.java
@@ -9,6 +9,7 @@ public class QuizApplication {
 
 	public static void main(String args[]) throws SQLException {
 		QuizApplication app = new QuizApplication();
+		System.out.println("WELCOME TO QUIZ APPLICATION!!!");
 		app.getMenu();
 
 	}
